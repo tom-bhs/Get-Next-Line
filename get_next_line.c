16adf25f4fe8c878e6c1cfc3c3b118063524ed4a
Tom BIHOUES <Tom.bihouess@gmail.com>
2023-11-30 02:08:08 +0100

    add

--- get_next_line.c
+++ get_next_line.c
@@ -3,12 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbihoues <tbihoues@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:19:13 by tbihoues          #+#    #+#             */
-/*   Updated: 2023/11/17 18:26:03 by tbihoues         ###   ########.fr       */
+/*   Updated: 2023/11/29 18:56:18 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char    *get_next_line(int fd);
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int		i;
+	int		j;
+	char	*dest;
+
+	i = 0;
+	j = 0;
+	dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dest)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		dest[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		dest[i + j] = s2[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
 
---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 18:07:33 by tom               #+#    #+#             */
+/*   Updated: 2023/11/29 18:53:36 by tom              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+char	*strjoin(const char *s1, const char *s2);
+char	*strdup(const char *s);
+size_t	strlen(const char *s);
+char	*substr(const char *str, size_t start, size_t length);
+char	*strchr(const char *s, int c);
+char	*_fill_line_buffer(int fd, char *left_c, char *buffer);
+char	*set_line(char *line_buffer);
+
+#endif
