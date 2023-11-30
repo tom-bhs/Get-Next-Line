4dd1d96ee651c62fad77f2b3d81d565008e5fb16
Tom BIHOUES <Tom.bihouess@gmail.com>
2023-11-30 23:35:18 +0100

    add

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,38 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 18:05:15 by tom               #+#    #+#             */
-/*   Updated: 2023/11/29 18:05:17 by tom              ###   ########.fr       */
+/*   Updated: 2023/11/30 23:35:03 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	int		size;
+	char	*dest;
+
+	i = 0;
+	size = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * size + 1);
+	if (!dest)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+size_t	strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
